badbc5fb90e4c8ce0aca88b3b73336de9aed110a
lucianogiacomelli <lucianogiacomelli8@gmail.com>
2026-08-04 21:23:26 -0300

    Create SecurityConfig.java

---
+++ src/main/java/com/labortrack/security/Security/Config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.labortrack.security.Security.Config;
+
+
+import com.labortrack.security.Security.Filter.JwtTokenValidator;
+import com.labortrack.security.Utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity //Permite que en el controller se defina que permiso o rol tiene acceso
+public class SecurityConfig {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(Customizer.withDefaults())
+                .addFilterBefore(new JwtTokenValidator(jwtUtils), BasicAuthenticationFilter.class)
+                .build();
+    }
+    /*
+    --------------------------****--------------------------
+    El AuthenticationManager es la interfaz principal
+    que recibe las credenciales enviadas por el usuario (usuario y contraseña)
+    y coordina el proceso para validar si son correctas.
+     --------------------------****--------------------------
+     */
+    @Bean
+    public AuthenticationManager authenticationManager
+    (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider (UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+
+        return new BCryptPasswordEncoder();
+    }
+
+}
